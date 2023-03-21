5a4f9bf324b2228cf23ee7c692236a70fef5b94e
sathish19961412 <sathishfreelanc5@gmail.com>
2023-03-21 10:14:21 +0530

    window object property and method

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// window.open('https://google.com');
+// window.close();
+//console.log(window.innerHeight,window.innerWidth);
+// window.moveTo(500,100);
+//window.resizeTo(1000,600);
+//window.alert('test');
+// window.prompt('what is your name ?');
+// console.log(window)
+console.log(console);
