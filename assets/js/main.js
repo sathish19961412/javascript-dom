8769955bf8acb5b7b04c193de0ef6e8ca25f8c24
sathish19961412 <sathishfreelanc5@gmail.com>
2023-03-21 11:52:17 +0530

    Brower object models used in screen,location,history,navigator

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,30 @@
 //window.alert('test');
 // window.prompt('what is your name ?');
 // console.log(window)
-console.log(console);
+// console.log(console);
+
+//window Screen
+//console.log(screen.width,screen.height);
+
+//window Location
+// console.log(location);
+// setInterval(()=>{
+//   location.href="https://google.com";
+//      location.assign("https://facebook.com");
+// },5000)
+
+// if(location.protocol === "http"){
+//      console.log("Unsecured: Your are browsing http.")
+// }else{
+//     console.log("secured: Your are browsing https.")
+// }
+
+//History
+// console.log(history);
+// history.back();
+// history.forward();
+// history.go();
+
+//Navigator
+// console.log(navigator);
+console.log(navigator.platform);
