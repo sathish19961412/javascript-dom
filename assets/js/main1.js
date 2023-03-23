a54e3c7306277396543d97044c8d26561b0256bb
sathish19961412 <sathishfreelanc5@gmail.com>
2023-03-23 10:46:20 +0530

    Expolre Dom Documents Using Js

---
+++ assets/js/main1.js
@@ -0,0 +1,35 @@
+// window.open('https://google.com');
+// window.close();
+//console.log(window.innerHeight,window.innerWidth);
+// window.moveTo(500,100);
+//window.resizeTo(1000,600);
+//window.alert('test');
+// window.prompt('what is your name ?');
+// console.log(window)
+// console.log(document);
+// console.log(consloe);
+//window Screen
+//console.log(screen.width,screen.height);
+
+//window Location
+// console.log(location);
+// setInterval(()=>{
+//   location.href="https://google.com";
+//      location.assign("https://facebook.com");
+// },5000)
+
+// if(location.protocol === "http"){
+//      console.log("Unsecured: Your are browsing http.")
+// }else{
+//     console.log("secured: Your are browsing https.")
+// }
+
+//History
+// console.log(history);
+// history.back();
+// history.forward();
+// history.go();
+
+//Navigator
+// console.log(navigator);
+// console.log(navigator);
---
+++ assets/js/main2.js
@@ -0,0 +1,10 @@
+
+// console.dir(document);
+// console.dir(document.domain);
+// console.log(document.URL)
+// console.log(document.title)
+// console.log(document.title="Welcome TO JsDom")
+// console.dir(document.head)
+// console.dir(document.body)
+// console.log(document.links)
+// console.log(document.all)
