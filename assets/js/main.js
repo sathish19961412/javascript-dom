c20a9c76c95b8b9efdef2a280ff6be8e579727e6
sathish19961412 <sathishfreelanc5@gmail.com>
2023-04-04 10:22:14 +0530

    work to async and defer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// const myDoc=document;
-// myDoc.body.children[0].children[0].innerText="Learn HTML DOM in JavaScript in Tamil"
-// console.log(myDoc)
-console.log(window.find("HTML DOM"))
+const myDoc=document;
+myDoc.body.children[0].innerText="Learn HTML DOM in JavaScript in Tamil"
+console.log(myDoc)
+console.log(window.find(alert()))
