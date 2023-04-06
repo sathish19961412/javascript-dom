3dda208bf4a20bd32bf4513822c84be4c5709078
sathish19961412 <sathishfreelanc5@gmail.com>
2023-04-06 10:47:44 +0530

    Dom Node Understand

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
-const myDoc=document;
-myDoc.body.children[0].innerText="Learn HTML DOM in JavaScript in Tamil"
-console.log(myDoc)
-console.log(window.find(alert()))
+// const myDoc=document;
+// myDoc.body.children[0].innerText="Learn HTML DOM in JavaScript in Tamil"
+// console.log(myDoc)
+// console.log(window.find(alert()))
+
+//Understand Dom Elements
+console.dir(document.documentElement.lang)
+console.dir(document.head);
+console.dir(document.body);
+
+console.log(document.body.constructor.name)
+console.log(document.head instanceof HTMLBodyElement)
+console.log(document.body instanceof HTMLElement)
+console.log(document.body instanceof Node)
+console.log(document.body instanceof EventTarget)
+
+console.log(document.body.nodeType)
+console.log(document.body.nodeName)
